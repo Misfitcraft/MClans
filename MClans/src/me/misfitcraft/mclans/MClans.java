130d581222db07f6e1bc64c5046eb6f8814bc890
tommycake50 <tommycake50@server.fake>
2013-07-12 22:31:59 +0100

    Added file checking to the main class

--- MClans/src/me/misfitcraft/mclans/MClans.java
+++ MClans/src/me/misfitcraft/mclans/MClans.java
@@ -1,7 +1,23 @@
 package me.misfitcraft.mclans;
 
+import java.io.File;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MClans extends JavaPlugin {
+	public File clansfile;
+	
+	@Override
+	public void onEnable() {
+		checkFiles();
+	}
 
+	private void checkFiles() {
+		//we are gonna have a config at some point
+		saveDefaultConfig();
+		clansfile = new File(getDataFolder(), "Clans");
+		if(!clansfile.exists()){
+			clansfile.mkdir();
+		}
+	}
 }
