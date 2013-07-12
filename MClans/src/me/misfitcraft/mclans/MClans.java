8f23f99712bc215f0a7177c45203345deeead96b
tommycake50 <tommycake50@server.fake>
2013-07-12 22:56:42 +0100

    Modified main class: added list of clans and started method for loading them

--- MClans/src/me/misfitcraft/mclans/MClans.java
+++ MClans/src/me/misfitcraft/mclans/MClans.java
@@ -6,10 +6,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MClans extends JavaPlugin {
 	public File clansfile;
+	public String[] clans;
 	
 	@Override
 	public void onEnable() {
 		checkFiles();
+		loadClans();
+	}
+
+	private void loadClans() {
+		//TODO either have the files with no extension or add functionality to .remove() the extensions
+		clans = clansfile.list();
+		//TODO add functionality to load clans from probably a text file in the clans folder
 	}
 
 	private void checkFiles() {
